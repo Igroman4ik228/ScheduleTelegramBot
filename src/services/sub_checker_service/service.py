5086df6adf4190d73ee9d1aab81da19d50d878a6
Vital <Vital@DESKTOP-K2UT91P>
2024-10-21 19:22:59 +0300

    Add SubCheckerSirvice

---
+++ src/services/sub_checker_service/service.py
@@ -0,0 +1,24 @@
+
+from background_service_pack.models import BackgroundService
+
+
+class SubCheckerSirvice(BackgroundService):
+    def __init__(self, time_span: int):
+        super().__init__(time_span)
+
+    async def do_work(self):
+        # ToDo: check user`s sub and edit data if sub is not active
+        pass
+    
+    async def active(self):
+        self.logger.info("SubCheckerSirvice active")
+        await super().active()
+
+    async def pause(self):
+        self.logger.info("SubCheckerSirvice paused")
+        await super().pause()
+
+    async def stop(self):
+        self.logger.info("SubCheckerSirvice stopped")
+        await self.pause()
+
