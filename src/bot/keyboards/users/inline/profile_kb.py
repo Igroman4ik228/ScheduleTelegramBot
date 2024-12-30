f02b5eca5b1e2922095d88f419b7e10f64f667f5
igroman4ik <ni4000009@gmail.com>
2024-12-30 22:38:43 +0300

    add emoji in profile kb

--- src/bot/keyboards/users/inline/profile_kb.py
+++ src/bot/keyboards/users/inline/profile_kb.py
@@ -5,7 +5,7 @@ from utils.constants import CallbackData
 
 def get_profile_kb() -> InlineKeyboardMarkup:
     profile_kb = [
-        [InlineKeyboardButton(text="Стандартное расписание",
+        [InlineKeyboardButton(text="📅 Стандартное расписание",
                               callback_data=CallbackData.WRITE_DEFAULT_SCHEDULE.value)],
         [InlineKeyboardButton(text="⚙️ Настройки",
                               callback_data=CallbackData.SETTING.value),
