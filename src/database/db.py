9da17bf0e7ed7764617b0d7791abe03a12e8c10e
igroman4ik <ni4000009@gmail.com>
2025-01-02 22:12:08 +0300

    delete unused func change decorator with_session

--- src/database/db.py
+++ src/database/db.py
@@ -5,7 +5,6 @@ from typing import AsyncGenerator
 from sqlalchemy.ext.asyncio import (AsyncSession, async_sessionmaker,
                                     create_async_engine)
 
-from database.models.base import Base
 from utils.config import settings
 
 
@@ -39,16 +38,6 @@ class DatabaseHelperAlchemy:
         async with self.sessionmaker() as session:
             yield session
 
-    async def create_tables(self) -> None:
-        async with self.engine.begin() as conn:
-            await conn.run_sync(Base.metadata.create_all)
-        await self.engine.dispose()
-
-    async def delete_tables(self) -> None:
-        async with self.engine.begin() as conn:
-            await conn.run_sync(Base.metadata.drop_all)
-        await self.engine.dispose()
-
     async def dispose(self):
         await self.engine.dispose()
 
@@ -63,11 +52,24 @@ db_helper = DatabaseHelperAlchemy(
 
 
 def with_session(func):
+    """
+    Декоратор для автоматического управления сессией базы данных.
+
+    Если сессия уже передана в аргументах - использует её.
+    Иначе создает новую сессию и передает её в декорируемую функцию.
+    """
     @wraps(func)
     async def wrapper(*args, **kwargs):
-        is_method = len(args) > 0 and hasattr(args[0], '__class__')
+        if 'session' in kwargs:
+            return await func(*args, **kwargs)
+
+        if any(isinstance(arg, AsyncSession) for arg in args):
+            return await func(*args, **kwargs)
+
         async with db_helper.get_session() as session:
+            is_method = args and hasattr(args[0], '__class__')
             if is_method:
                 return await func(args[0], *args[1:], session=session, **kwargs)
             return await func(*args, session=session, **kwargs)
+
     return wrapper
